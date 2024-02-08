4b185615d10871486e0d1e9f29cf24a2834add51
Alfonso Exerweb <alfonso.exer@gmail.com>
2024-02-08 14:08:10 -0600

    1.1.1
    Added prompt for options

--- index.js
+++ index.js
@@ -1,39 +1,103 @@
-const version = "1.0.1";
+const version = "1.1.1";
 const puppeteer = require("puppeteer");
 const {execSync} = require('child_process');
+const prompt = require('prompt-sync')();
+
+let mainUrl = '';
+let deepness = 3;
+let verbose = true;
+let omitPrefix = '';
+let importanceMap = ["1.0", "0.9", "1", "0.5"];
+
 const fs = require('fs');
 let urls = [];
 let titles = [];
 let levels = [];
 let browser, page;
 let index = 0;
-
-const mainUrl = 'https://www.alfnigredotattoos.com/';
-const waitTime = 5;
-const deepness = 3;
-const verbose = false;
-const omitPrefix = 'admin/';
-const importanceMap = ["1.0", "0.9", "0.5", "0.2"];
+let time = 0;
+let start, end = 0;
 
 async function main(){
+	start = new Date().getTime();
 	console.clear();
-	console.log("\x1b[34mNigredo Crawler v" + version);
-	console.log("\x1b[33mCrawling... please wait patiently...\x1b[0m");
+	console.log("  __    _     ____      _      _   __    ___   ____  ___   ___       _  _____");
+	console.log(" / /\\  | |   | |_      | |\\ | | | / /`_ | |_) | |_  | | \\ / / \\     | |  | | ");
+	console.log("/_/--\\ |_|__ |_|       |_| \\| |_| \\_\\_/ |_| \\ |_|__ |_|_/ \\_\\_/     |_|  |_| ");
+	console.log("");
+	console.log("\x1b[34mNigredo Crawler v" + version + '\x1b[0m');
+	console.log("");
+	console.log("The Nigredo Crawler Sitemap Generator Console Application is a powerful tool designed to create sitemaps for websites that utilize dynamic content rendering, such as those built with Angular or similar JavaScript frameworks. Unlike traditional web crawlers that rely solely on parsing the DOM (Document Object Model) of a webpage, this application employs a sophisticated approach to discover and map out dynamic content.");
+	console.log("");
 
-	urls.push(mainUrl);
-	levels[0] = 0;
+	let startCrawl = true;
+	
+	console.log("Please fill the options to start the crawler");
+	console.log("Remember to include the protocol (http or https) in the starting URL");
+	mainUrl = prompt('Starting URL (required): ');
+	if (!mainUrl.endsWith("/")) mainUrl = mainUrl + '/';
 
-	browser = await puppeteer.launch();
-	page = await browser.newPage();
+	deepness = parseInt(prompt('Deepness (required): '));
+	
+	console.log("If you want to avoid the program to craw a group of pages, please write the prefix of it");
+	omitPrefix = prompt("Prefix to Omit: ");
+	
+	console.log("The importance map is a list of comma separated floats of the levels of deepness");
+	console.log("* You must write a list of " + (deepness + 1) + " numbers");
+	importanceMap = prompt("Importance Map (required): ");
+	const arrMap = importanceMap.split(",");
+	importanceMap = arrMap.map((v) => v.trim());
+	
+	const verboseInput = prompt('Verbose (Y | N): ');
+	if (verboseInput.toLowerCase() == "y") verbose = true;
+	else verbose = false;
 
-	crawlPage(mainUrl);
+	if (mainUrl == "" || deepness <= 0 || isNaN(deepness) || importanceMap.length == 0) startCrawl = false;
+
+	if (startCrawl){
+		console.log("\x1b[33mCrawling... please wait patiently...\x1b[0m");
+		console.log("");
+
+		urls.push(mainUrl);
+		levels[0] = 0;
+
+		browser = await puppeteer.launch();
+		page = await browser.newPage();
+
+		crawlPage(mainUrl);
+	}else{
+		console.log("\x1b[31mERROR: Please fill all required options\x1b[0m");
+		prompt('Press any key to start again')
+		main();
+	}
 }
-async function crawlPage(url){ 
-	await page.goto(url);
+async function crawlPage(url){
+	if(verbose) console.log("Page \x1b[34murl: " + url + "\x1b[0m");
+	page.setCacheEnabled(false);
+	await page.goto(url, {
+		waitUntil: ['load', 'domcontentloaded', 'networkidle0'],
+	});
 	await page.setViewport({width: 1024, height: 800});
-	await page.waitForTimeout(waitTime * 1000);
+	// await page.waitForTimeout(waitTime * 1000);
 	const pageTitle = await page.title();
-	if(verbose) console.log("Page title: " + pageTitle);
+	if(verbose){
+		const metrics = await page.metrics();
+		console.log("Page \x1b[34mtitle:\x1b[0m " + pageTitle);
+		execSync('sleep 0.1');
+		
+		console.log("\x1b[33mMetrics Documents:\x1b[0m " + metrics.Documents);
+		execSync('sleep 0.1');
+		console.log("\x1b[33mMetrics Frames:\x1b[0m " + metrics.Frames);
+		execSync('sleep 0.1');
+		console.log("\x1b[33mMetrics Nodes:\x1b[0m " + metrics.Nodes);
+		execSync('sleep 0.1');
+		console.log("\x1b[33mMetrics LayoutCount:\x1b[0m " + metrics.LayoutCount);
+		execSync('sleep 0.1');
+		console.log("\x1b[33mMetrics LayoutDuration:\x1b[0m " + metrics.LayoutDuration);
+		execSync('sleep 0.1');
+		console.log("\x1b[33mMetrics TaskDuration:\x1b[0m " + metrics.TaskDuration);
+		execSync('sleep 0.1');
+	}
 	titles[index] = pageTitle;
 
 	const elements = await page.$$('a');
@@ -46,16 +110,23 @@ async function crawlPage(url){
 			if(verbose) console.log("-- \x1b[32mADDED\x1b[0m: " + link);
 			urls.push(link);
 			levels.push((levels[index]+1));
-			execSync('sleep 1');
+			execSync('sleep 0.2');
 		}
 	}
 
+	if (verbose) console.log("------------------------");
 	index++;
 	if(index < urls.length){
 		crawlPage(urls[index]);
-	}else{
+	} else {
+		console.log("");
 		console.log("\x1b[31mCrawling Finished");
-		console.log("Discovered " + urls.length + " pages\x1b[0m");
+
+		end = new Date().getTime();
+		minutes = parseInt(((end - start) / 1000) / 60);
+		seconds = parseInt(((end - start) / 1000) - (minutes * 60));
+		time = minutes.toString().padStart(2, "0") + ':' + seconds.toString().padStart(2, "0");
+		console.log("Discovered " + urls.length + " pages in " + time + "\x1b[0m");
 		// if(verbose) console.log(urls);
 		await page.close();
 		await browser.close();
@@ -82,7 +153,7 @@ function createXml(){
 	console.log("\x1b[33mWriting XML\x1b[0m");
 	fs.writeFile('sitemap.xml', xml, err => {
 	  if (err) {
-		console.log("\x1b[32mError Ocurred\x1b[0m");
+		console.log("\x1b[31mError Ocurred\x1b[0m");
 	    console.error(err);
 	  }
 		console.log("\x1b[32mXML Sitemap Saved\x1b[0m");
